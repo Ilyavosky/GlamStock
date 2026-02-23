a83110a1961f9908b606799388b19b43a65a3394
Ilyavosky <ilyacortesruiz@gmail.com>
2026-02-22 20:03:56 -0600

    Implementar intefaces para hooks

--- glamstock/src/types/inventario-view.types.ts
+++ glamstock/src/types/inventario-view.types.ts
@@ -21,3 +21,24 @@ export interface ProductoFila {
   valorVenta: number;
   sucursal: string;
 }
+
+export interface VarianteDetalle {
+  id_variante: number;
+  codigo_barras: string;
+  modelo: string | null;
+  color: string | null;
+  precio_adquisicion: number;
+  precio_venta_etiqueta: number;
+}
+
+export interface ProductoCompleto {
+  id_producto_maestro: number;
+  sku: string;
+  nombre: string;
+  variantes: VarianteDetalle[];
+}
+
+export interface InventarioInfo {
+  sucursal: string;
+  stock_actual: number;
+}
